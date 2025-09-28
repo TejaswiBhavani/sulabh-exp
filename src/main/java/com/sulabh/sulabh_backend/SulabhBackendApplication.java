1b41dfcac292a984467bafbbc31c96935173e249
TejaswiBhavani <tejaswibhavanih36@gmail.com>
2025-09-28 18:34:36 +0530

    Initial commit: replace old backend with Spring Boot skeleton

---
+++ src/main/java/com/sulabh/sulabh_backend/SulabhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sulabh.sulabh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulabhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulabhBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulabh/sulabh_backend/SulabhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulabh.sulabh_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulabhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
